11b1baa916706ff63220b42793b7068627722490
rishabh0x00 <rsharma0x00@gmail.com>
2025-02-12 19:47:25 +0530

    fix: update import path

--- test/MinimalDEX.t.sol
+++ test/MinimalDEX.t.sol
@@ -2,8 +2,8 @@
 pragma solidity 0.8.28;
 
 import "forge-std/Test.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "../src/MinimalDEX.sol";
-import "openzeppelin-contracts/token/ERC20/ERC20.sol";
 
 contract MockERC20 is ERC20 {
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
